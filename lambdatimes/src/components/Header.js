6f8fb3fd8478cdab5b3f42a2d591344e98132435
ajb85 <ajbrush85@gmail.com>
2019-02-22 11:38:15 -0500

    Completed methods for Content.js plus added props to <Tabs/>

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
     <div className="header">
+      {/* Is it SMARCH already? */}
       <span className="date">SMARCH 32, 2018</span>
       <h1>Lambda Times</h1>
       <span className="temp">98°</span>
