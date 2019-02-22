58e928bcd6daf15b0dcdb4c2a0220df8faaffee8
ajb85 <ajbrush85@gmail.com>
2019-02-22 11:54:53 -0500

    Card.js now displays props data.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -3,7 +3,10 @@ import Card from "./Card";
 // props: cards={this.filterCards()}
 const Cards = props => {
   // I like to keep my JavaScript out of my JSX because I, personally, find it easier to read
-  const cards = props.cards.map(card => <Card card={card} />);
+  let cards = props.cards.map(card => <Card key={card.headline} card={card} />);
+  if (!cards) {
+    cards = "No cards to display";
+  }
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a
