ee5dd8b72bbb9056292fa9b6890cad5b576f2a30
ajb85 <ajbrush85@gmail.com>
2019-02-22 11:42:48 -0500

    Cards.js now maps over props and displays the results

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import Card from './Card';
-
+import React, { Component } from "react";
+import Card from "./Card";
+// props: cards={this.filterCards()}
 const Cards = props => {
+  // I like to keep my JavaScript out of my JSX because I, personally, find it easier to read
+  const cards = props.cards.map(card => <Card card={card} />);
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {/* Using the cards prop, map over the list creating a
           new Card component for each passing the card as the only prop*/}
+      {cards}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
